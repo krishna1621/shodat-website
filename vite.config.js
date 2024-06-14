09172464449b39f5e18862530235bd37e6fb6c17
krishna1621 <115209389+krishna1621@users.noreply.github.com>
2024-06-14 14:47:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api/schedule-meeting": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-      "/api/message": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-      "/api/business_email": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [react()],
+  base: '/shodat-website/', // Replace 'REPO_NAME' with your repository name
+  build: {
+    outDir: 'dist'
+  }
 });
