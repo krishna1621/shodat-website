ff9e15153e4b980533d62d3d727a07acc827cb0c
krishna1621 <115209389+krishna1621@users.noreply.github.com>
2024-06-27 14:45:32 +0530

    new file added

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api/schedule-meeting": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-      "/api/message": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-      "/api/business_email": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [react()],
-});
+ 
+})
